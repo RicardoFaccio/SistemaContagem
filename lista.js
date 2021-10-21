c6d59eeec8722a591b13e5c4bf23c366d94b4771
RicardoFaccio <ricardofac2005@hotmail.com>
2021-10-20 23:09:09 -0300

    adicionado arquivo de código do sistema

--- lista.js
+++ lista.js
@@ -1,13 +1,15 @@
 let numeroAlunos = 5
 
+let listaAlunos = ["ZERO", "Adriano", "Isa", "Ricardo","Aline"]
+
 for (let contador = 0; contador < numeroAlunos; contador++) {
 
 
     if (contador == 0) {
-        console.log(contador + ": ZERO") // Concatenação
+        console.log(contador + ": ZERO: " + listaAlunos [contador]) // Concatenação
     } else if (contador % 2 == 1) {
-        console.log(`${contador}: ÍMPAR`) // Interpolação - Template Strings
+        console.log(`${contador}: ÍMPAR: ${listaAlunos [contador]}`) // Interpolação - Template Strings
     } else {
-        console.log(`${contador}: PAR`)
+        console.log(`${contador}: PAR: ${listaAlunos [contador]}`)
     }
 }
